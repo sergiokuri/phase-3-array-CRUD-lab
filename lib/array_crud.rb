4693eb0198c8472f1af3ad5f99c3fca36e3117e2
Sergio <kurisergio777777@gmail.com>
2022-05-20 12:33:37 -0400

    commit tree e83cd49b284b36e5cbfa9acaf36c9673847053c5

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
+    emp_array = []
   
 end
 
 def create_an_array
+    four_el = ["sergio", "andres", "eduardo", "magaly"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+    array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
+    array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
+    array.pop()
   
 end
 
 def remove_element_from_start_of_array(array)
+    array.shift()
   
 end
 
 def retrieve_element_from_index(array, index_number)
+    array[index_number] = "am"
   
 end
 
 def retrieve_first_element_from_array(array)
+    array[1]= "wow"
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
